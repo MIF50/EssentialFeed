cb373a48a67c524a0b7b0235aac5fa2302222544
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-03-31 07:53:33 +0300

    Inserting to non-empty cache overrides previously inserted cache values

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -130,6 +130,20 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .failure(anyNSError()))
     }
     
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        let sut = makeSUT()
+        
+        let firstInsertionError = insert((uniqueImageFeed().local,Date()), to: sut)
+        XCTAssertNil(firstInsertionError,"Expected to inserted cache successfully")
+        
+        let latestFeed = uniqueImageFeed().local
+        let latestTimestamp = Date()
+        let latestInsertionError = insert((latestFeed,latestTimestamp), to: sut)
+        XCTAssertNil(latestInsertionError,"Expected to override cache successfully")
+        
+        expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(storeURL: URL? = nil,
@@ -140,13 +154,19 @@ class CodableFeedStoreTests: XCTestCase {
         return sut
     }
     
-    private func insert(_ cache:(feed: [LocalFeedImage],timestamp: Date),to sut: CodableFeedStore) {
+    @discardableResult
+    private func insert(_ cache:(feed: [LocalFeedImage],timestamp: Date),
+                        to sut: CodableFeedStore)-> Error? {
         let exp = expectation(description: "Wait for cache insertion")
+        
+        var receivedError: Error?
         sut.insert(cache.feed,timestamp: cache.timestamp) { insertionError in
-            XCTAssertNil(insertionError,"Expected feed to be inserted successfully")
+            receivedError = insertionError
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
+        
+        return receivedError
     }
     
     private func expect(_ sut: CodableFeedStore,
