0af8986f17270bf07d190f4f81592e11f7dd7725
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-04-04 23:14:07 +0300

    'LocalFeedLoader' in integration with the 'CoreDataFeedStore' overrides items saved by separate instances, proving we correctly manage the data models on disk.

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -43,6 +43,30 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         expect(sutToPerformLoad, toLoad: feed)
     }
     
+    func test_save_overridesItemsSavedOnASeparateInstance() {
+        let sutToPerformFirstSave = makeSUT()
+        let sutToPerformLastSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let firstFeed = uniqueImageFeed().models
+        let latestFeed = uniqueImageFeed().models
+
+        let saveExp1 = expectation(description: "Wait for save completion")
+        sutToPerformFirstSave.save(firstFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp1.fulfill()
+        }
+        wait(for: [saveExp1], timeout: 1.0)
+
+        let saveExp2 = expectation(description: "Wait for save completion")
+        sutToPerformLastSave.save(latestFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp2.fulfill()
+        }
+        wait(for: [saveExp2], timeout: 1.0)
+
+        expect(sutToPerformLoad, toLoad: latestFeed)
+    }
+    
     // MARK: - Helper
     
     private func makeSUT(file: StaticString = #filePath,
@@ -65,7 +89,7 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
         sut.load { result in
             switch result {
             case let .success(loadedFeed):
-                XCTAssertEqual(loadedFeed, expectedFeed)
+                XCTAssertEqual(loadedFeed, expectedFeed,file: file,line: line)
                 
             case let .failure(error):
                 XCTFail("Expected successful feed result, got \(error) instead",file: file,line: line)
