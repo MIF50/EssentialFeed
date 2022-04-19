7cc55bbbc5382790095586b55454db9cc7d906da
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-04-20 00:34:01 +0300

    Nest LoadFeedResult into the FeedLoader protocol as 'FeedLoader.Result' since they're closely related

--- EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -14,7 +14,7 @@ public class RemoteFeedLoader: FeedLoader {
         case invalidData
     }
     
-    public typealias Result = LoadFeedResult
+    public typealias Result = FeedLoader.Result
     
     private let url: URL
     private let client: HTTPClient
