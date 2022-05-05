55d1e880c201569f2d2eb1a57bdb510f1883e9c4
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-05-05 05:18:57 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Mohamed Ibrahim on 04/05/2022.
+//
+
+import UIKit
+
+final public class FeedImageCell: UITableViewCell {
+    
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
