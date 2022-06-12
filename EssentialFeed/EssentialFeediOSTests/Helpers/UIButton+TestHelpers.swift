8e0e55207c2df93ef3546f8ff92118d9b7b67e8b
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-06-12 22:46:41 +0300

    Extract duplicate logic into a reusable UIControl extension

--- EssentialFeed/EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -10,10 +10,6 @@ import UIKit
 extension UIButton {
     
     func simulateTap() {
-        self.allTargets.forEach { target in
-            self.actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
+        simulate(event: .touchUpInside)
     }
 }
--- EssentialFeed/EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIRefreshControl+TestHelpers.swift
@@ -10,10 +10,6 @@ import UIKit
 extension UIRefreshControl {
     
     func simulatePullToRefresh() {
-        self.allTargets.forEach { target in
-            self.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
+        simulate(event: .valueChanged)
     }
 }
