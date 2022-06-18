fcf165d6345b3902d69bee64196e2a00ae746746
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-06-18 14:32:24 +0300

    Decouple FeedImageViewModel from UIKit by creating a transformation closure that converts an image `Data` value into a generic `Image` type. When composing with a UIKit user interface, we inject a closure to transform the image `Data` into `UIImage`.

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -6,6 +6,7 @@
 //
 
 import EssentialFeed
+import UIKit
 
 public final class FeedUIComposer {
     private init() { }
@@ -21,7 +22,10 @@ public final class FeedUIComposer {
     private static func adaptFeedToCellControllers(forwarding controller: FeedViewController,loader: FeedImageDataLoader) -> (([FeedImage]) -> Void) {
         return { [weak controller] feed in
             controller?.tableModel = feed.map {  model in
-                FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: loader))
+                FeedImageCellController(viewModel: FeedImageViewModel(model: model,
+                                                                      imageLoader: loader,
+                                                                      imageTransformer: UIImage.init(data:))
+                )
             }
         }
     }
