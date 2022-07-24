3c237bb6e899741f86fd94189e48d4c61d439bbd
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-07-24 23:03:58 +0300

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingView.swift
+//  EssentialFeediOS
+//
+//  Created by Mohamed Ibrahim on 24/07/2022.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Mohamed Ibrahim on 24/07/2022.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
