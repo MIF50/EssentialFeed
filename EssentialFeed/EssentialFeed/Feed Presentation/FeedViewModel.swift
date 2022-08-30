bd02f049a5828a83e1c7485a758be83a2ef43f87
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-08-30 21:52:32 +0300

    Move FeedViewModel to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mohamed Ibrahim on 30/08/2022.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
