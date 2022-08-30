97b47f91e1d245a37d19dfa157d04e181a08025e
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-08-30 21:32:17 +0300

    FeedPresenter displays feed and stops loading on `didFinishLoadingFeed`

--- EssentialFeed/EssentialFeed/Feed Feature/FeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImage.swift
@@ -7,16 +7,18 @@
 
 import Foundation
 
-public struct FeedImage: Equatable {
+public struct FeedImage: Hashable {
     public let id: UUID
     public let description: String?
     public let location: String?
     public let url: URL
     
-    public init(id: UUID,
-                description: String?,
-                location: String?,
-                url: URL) {
+    public init(
+        id: UUID,
+        description: String?,
+        location: String?,
+        url: URL
+    ) {
         self.id = id
         self.description = description
         self.location = location
