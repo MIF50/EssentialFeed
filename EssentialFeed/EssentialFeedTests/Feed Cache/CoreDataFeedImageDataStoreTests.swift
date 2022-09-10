0f0e030e496b9b2d1c4b24d8188ef5d6e8257b71
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-09-10 18:50:36 +0300

    CoreDataFeedStore.retrieveImageData delivers last inserted value (overwriting previous values)

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -36,6 +36,18 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrieveWith: found(storeData), for: url)
     }
     
+    func test_retrieveImageData_deliversLastInsertedValue() {
+        let sut = makeSUT()
+        let firstStoredData = Data("first".utf8)
+        let lastStoredData = Data("last".utf8)
+        let url = URL(string: "http://a-url.com")!
+
+        insert(firstStoredData, for: url, into: sut)
+        insert(lastStoredData, for: url, into: sut)
+        
+        expect(sut, toCompleteRetrieveWith: found(lastStoredData), for: url)
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(
