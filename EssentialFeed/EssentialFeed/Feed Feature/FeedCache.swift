047d766a796bf57f9e78db99777aa9ddb6e0d46c
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-09-17 14:32:20 +0300

    Make the LocalFeedLoader implement the FeedCache protocol (so it can be composed)

--- EssentialFeed/EssentialFeed/Feed Feature/FeedCache.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedCache.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public protocol FeedCache {
-    typealias Result = LocalFeedLoader.SaveResult
+    typealias Result = Swift.Result<Void,Error>
     
     func save(_ feed: [FeedImage],completion: @escaping ((Result)-> Void))
 }
