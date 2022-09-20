415ed3cef71c4023ca0bec226aae2ad136d3627a
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-09-20 22:27:51 +0300

    Add UI Test target for running high-level Acceptance Tests

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -18,19 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         
-        let httpClient: HTTPClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let localFeedStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feed-store.sqlite")
-        let store = try! CoreDataFeedStore(storeURL: localFeedStoreURL)
-        let localFeedLoader = LocalFeedLoader(store: store, currentDate: Date.init)
-        let localImageDataLoader = LocalFeedImageDataLoader(store: store)
-        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        let remoteFeedLoader = RemoteFeedLoader(url: url, client: httpClient)
-        let remoteImageDataLoader = RemoteFeedImageDataLoader(client: httpClient)
-        let feedLoader = FeedLoaderWithFallbackComposite(primary: remoteFeedLoader, fallback: localFeedLoader)
-        let imageLoader = FeedImageDataLoaderWithFallbackComposite(primary: localImageDataLoader, fallback: remoteImageDataLoader)
-        let feedViewController = FeedUIComposer.feedComposeWith(feedLoader: feedLoader,imageLoader: imageLoader)
-        window?.rootViewController = feedViewController
-        window?.makeKeyAndVisible()
     }
 
 }
