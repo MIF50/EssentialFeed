560a4bd88875dcb6cd411e12777b25dc00a64c7d
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-11-29 00:52:55 +0300

    Replace custom CellController protocol with a composition of the UITableViewDataSource/Delegate/Prefetching, so we can decouple other modules from the Shared UI module

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -11,5 +11,4 @@ final public class ImageCommentCell: UITableViewCell {
     @IBOutlet private(set) public var dateLabel: UILabel!
     @IBOutlet private(set) public var usernameLabel: UILabel!
     @IBOutlet private(set) public var messageLabel: UILabel!
-
 }
